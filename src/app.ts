7f6175fa160a1f2067cfe136709c1bb6e55d410a
bishtnitish <nbisht@connamara.com>
2025-05-10 15:07:43 +0530

    Code implementation: allowing diffrent delimiters

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ class App {
     static add(input: string) {
       let result = 0;
 
-      const delimeters = new RegExp([",", "\n"].join('|'), 'g')
+      const delimeters = new RegExp([",", "\n", "//", ";"].join('|'), 'g')
 
       input.split(delimeters).map((str) => {
         const number = Number(str.trim())
