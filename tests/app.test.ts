ae4186628e7b23516d2c184d8f30c7e34c6ce086
bishtnitish <nbisht@connamara.com>
2025-05-10 14:42:13 +0530

    Test Case: String "1, 5"

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -8,4 +8,8 @@ describe('App.add', () => {
   test('returns 1 for string 1', () => {
     expect(StringCalculator.add("1")).toBe(1);
   });
+
+  test('returns 6 for string 1, 5', () => {
+    expect(StringCalculator.add("1, 5")).toBe(6);
+  });
 });
