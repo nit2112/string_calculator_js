2c5a0a001644150ce98455042cf08df0f30a5ccf
bishtnitish <nbisht@connamara.com>
2025-05-10 14:40:32 +0530

    Code refactor: for string "1"

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 class App {
     static add(input: string) {
       let result = 0;
-      if (input === "") {
-        result = 0;
-      } else if(input === "1") {
+      if(input === "1") {
         result = 1;
       }
       return result;
